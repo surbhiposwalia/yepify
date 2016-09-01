dc25094983a9d7b6c46e4763db29ff39d5111ec6
surbhiposwalia <poswalia@ac87a31958e6.ant.amazon.com>
2016-08-31 22:35:52 -0700

    final copy

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var auth = {
     }
 }
 
+
 var accessor = {
     consumerSecret: auth.consumerSecret,
     tokenSecret: auth.accessTokenSecret
@@ -31,7 +32,7 @@ $(function() {
         event.preventDefault();
         loc = $('#location_id').val();
         terms = $('#cuisine_id').val();
-       fetchResults(loc, terms);
+        fetchResults(loc, terms);
     });
 });
 
@@ -58,33 +59,57 @@ function fetchResults(loc, terms) {
         'cache': true
     })
 }
+var resloc = [];
+
+function res_loc(latitude, longitude,name) {
+
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.name=name;
+
+}
 
 function cb(data) {
     console.log(data.businesses);
     displayResults(data.businesses);
 }
-function displayResults(data){
-  $.each(data,function(index,value){
-    var name=value.name;
-    var address=value.location.address[0];
-    var review_count=value.review_count;
-    var image=value.image_url;
-    var rating_image=value.rating_img_url;
-    $('#display').append("<p class=restauraunts> Name:<b>"+name+"</b><br><img src="+image+">Address: "+address+"<br>Review Count:"+review_count+"<img src="+rating_image+"> </p>")
-  });
 
 
+function displayResults(data) {
+    $.each(data, function(index, value) {
+        var lat = value.location.coordinate.latitude;
+        var long = value.location.coordinate.longitude;
+        var name=value.name;
+        var tempLocation = new res_loc(lat, long,name);
+        resloc.push(tempLocation);
+        var address = value.location.address[0];
+        var review_count = value.review_count;
+        var image = value.image_url;
+        var rating_image = value.rating_img_url;
+        $('#display').append("<p class=restaurants> Name:<b>" + name + "</b><br><img src=" + image + ">Address: " + address + "<br>Review Count:" + review_count + "<img src=" + rating_image + "></p>")
+    });
+}
 
+$(document).on('mouseenter','p',function(){
+    var i =$('.restaurants').index(this);
+    initMap(resloc[i].latitude,resloc[i].longitude,resloc[i].name);    
+})
+var map;
+
+function initMap(lat,lng,name) {
+    var myLatLng = { lat, lng };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 13
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: name
+    });
 }
 
 
 //value.location.name
 //value.location.rating,review_count, 
 //rating_img_url
-
-
-
-
-
-
-
